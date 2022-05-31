301b4d8bd4d184dd8222e6ee68186d8ef7609c49
abbott-czy <50007640+abbott-czy@users.noreply.github.com>
2022-05-31 15:38:57 +0800

    Update testModel.py

--- HGA_CDM/testModel.py
+++ HGA_CDM/testModel.py
@@ -5,10 +5,10 @@ from multiprocessing import Pool
 
 def testModel(n_students, n_questions, n_knowledge_coarse, n_knowledge_fine, len_s_g, data, q_matrix, slip, guess, data_patch_id, run_id=1,
               n_pop=50, max_generations=100, alg_name='GA_NBC', data_name='Math_DMiC'):
-    if n_knowledge_coarse == n_knowledge_fine:
-        multi = False
 
-        Accuracy, Precision, Recall, F1, AUC = predictDINA(data, q_matrix, slip, guess, multi,data_patch_id, run_id=1)
+    multi = False
+
+    Accuracy, Precision, Recall, F1, AUC = predictDINA(data, q_matrix, slip, guess, multi,data_patch_id, run_id=1)
 
 
     return Accuracy, Precision, Recall, F1, AUC
@@ -22,23 +22,14 @@ def predictDINA(data, q_matrix, slip, guess, multi,data_patch_id, run_id=1):
     data = np.array(data)
     q_matrix = np.mat(q_matrix)
     n_questions, n_knowledge = q_matrix.shape
-
-
+    # crate K matrix，indict k skill could get how many vector
     k_matrix = np.mat(np.zeros((n_knowledge, 2 ** n_knowledge), dtype=int))
     for j in range(2 ** n_knowledge):
         l = list(bin(j).replace('0b', ''))
         for i in range(len(l)):
             k_matrix[n_knowledge - len(l) + i, j] = l[i]
-
-
-
     std = np.sum(q_matrix, axis=1)
-
     r_matrix = (q_matrix * k_matrix == std) * 1
-
-
-    print('训练结束，预测开始')
-
     ni, nj = data.shape
     Qi, Qj = q_matrix.shape
 
@@ -46,7 +37,7 @@ def predictDINA(data, q_matrix, slip, guess, multi,data_patch_id, run_id=1):
     k = Qj
 
     if multi == True:
-        print('预测 multi 4 processes')
+        print(' multi 4 processes')
         with Pool(processes=4) as pool:
             multiple_results = [pool.apply_async(EStep_multi, (IL, slip, guess, data, r_matrix, k, i)) for i in range(4)]
             for item in ([res.get(timeout=1000) for res in multiple_results]):
@@ -61,7 +52,6 @@ def predictDINA(data, q_matrix, slip, guess, multi,data_patch_id, run_id=1):
 
     a = IL.argmax(axis=1)
 
-    p1 = np.sum((r_matrix[:, a] == data.T) * 1) / (i * j)
     rrrSum = []
     nnnSum = []
     for rrr, nnn in zip(r_matrix[:, a], data.T):
@@ -86,8 +76,8 @@ def predictDINA(data, q_matrix, slip, guess, multi,data_patch_id, run_id=1):
 
     Accuracy = (TP + TN) / (TP + TN + FP + FN)
 
-    print('预测消耗时间：' + str(int(time.time()) - int(startTime)) + '秒')
-    print('------------------预测结束-----------------------------')
+    print('The predicted model time is.：' + str(int(time.time()) - int(startTime)) + 's')
+    print('------------------Prediction End-----------------------------')
     return Accuracy, Precision, Recall, F1, AUC
 
 
