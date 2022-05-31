694837865bfd25d97f6c043673a6b4dc741e877a
abbott-czy <50007640+abbott-czy@users.noreply.github.com>
2022-05-31 15:32:07 +0800

    Update main.py

--- HGA_CDM/main.py
+++ HGA_CDM/main.py
@@ -19,17 +19,17 @@ def single_run(data_patch_id, run_id, num_train_students, n_questions, n_knowled
                data_train, q_matrix, data_test, num_test_students, alg_name, data_name, max_generations, n_pop):
     random.seed(run_id)
     f_record, f_record_data = files_open(alg_name, data_name, data_patch_id, run_id)
-    print("第" + str(data_patch_id) + "折" + "第" + str(run_id) + "次训练")
+    print("The" + str(data_patch_id) + "th patch" + ",The" + str(run_id) + "th training ")
 
     slip, guess = trainModel(num_train_students, n_questions, n_knowledge_coarse, n_knowledge_fine, len_s_g, data_train,
                              q_matrix, data_patch_id, run_id, n_pop, max_generations, alg_name, data_name)
 
-    print("第" + str(data_patch_id) + "折" + "第" + str(run_id) + "次测试")
+    print("The" + str(data_patch_id) + "th patch" + ",The" + str(run_id) + "th test ")
     accuracy, precision, recall, f1, auc = testModel(num_test_students, n_questions, n_knowledge_coarse,
                                                      n_knowledge_fine, len_s_g, data_test,
                                                      q_matrix, slip, guess, data_patch_id, run_id, n_pop,
-                                                     max_generations, alg_name, data_name)  # testModel.py  6行
-    f_record.writelines("第" + str(data_patch_id) + "折" + "第" + str(run_id) + "次" + "\n" + "Accuracy:" + str(accuracy) +
+                                                     max_generations, alg_name, data_name)
+    f_record.writelines("The" + str(data_patch_id) + "th patch" + ",The" + str(run_id) + "th" + "\n" + "Accuracy:" + str(accuracy) +
                         "\t" + "Precision:" + str(precision) + "\t" + "Recall:" + str(recall) + "\t" + "F1:" + str(f1) +
                         "\t" + "AUC:" + str(auc) + "\t" + "\n")
 
@@ -40,24 +40,10 @@ def single_run(data_patch_id, run_id, num_train_students, n_questions, n_knowled
     return accuracy, precision, recall, f1, auc, run_id
 
 
-def prepare(dataset_idx=0, granularity=0):
-
+def prepare(dataset_idx=0):
     data_name = data_names[dataset_idx]
-
-    if data_name == 'Math_DMiC':
-        q_matrix_0, q_matrix_1, q_matrix_2, data = data_reader_math_DMiC(data_name)
-
-        if granularity == 0:
-            q_matrix = q_matrix_0
-        elif granularity == 1:
-            q_matrix = q_matrix_1
-        elif granularity == 2:
-            q_matrix = q_matrix_2
-        q_matrix_coarse = q_matrix_0
-    else:
-        q_matrix, data = data_reader(data_name)
-        q_matrix_coarse = q_matrix
-    return data, q_matrix, q_matrix_coarse
+    q_matrix, data = data_reader(data_name)
+    return data, q_matrix
 
 
 def run_main(alg_name, data_name, data, q_matrix, max_runs, max_split, max_generations, n_pop, multi,
@@ -76,12 +62,12 @@ def run_main(alg_name, data_name, data, q_matrix, max_runs, max_split, max_gener
 
         num_train_students = len(data_train)
         num_test_students = len(data_test)
-        print('训练集学生数' + str(num_train_students))
-        print('测试集学生数' + str(num_test_students))
+        print('Number of students in the training set' + str(num_train_students))
+        print('Number of students in the test set' + str(num_test_students))
 
         n_questions = len(q_matrix)
         n_knowledge = len(q_matrix[0])
-        len_s_g = 7
+        len_s_g = 7  # binary encoding length
 
         n_knowledge_fine = n_knowledge
 
@@ -127,7 +113,7 @@ def run_main(alg_name, data_name, data, q_matrix, max_runs, max_split, max_gener
                 array_recall.append(recall)
                 array_f1.append(f1)
                 array_auc.append(auc)
-                results_per_run.append((run_id, accuracy, precision, recall, f1, auc))  # 每次的运行结果
+                results_per_run.append((run_id, accuracy, precision, recall, f1, auc))
         else:
             for run_id in range(max_runs):
 
@@ -172,18 +158,16 @@ def run_main(alg_name, data_name, data, q_matrix, max_runs, max_split, max_gener
 if __name__ == '__main__':
 
     startTimeA = time.time()
-    max_runs =20
+    max_runs =20   # number of independent runs
     max_split = 5
-    is_multi = True
-    max_generations = 50
-    n_pop = 100
+    is_multi = True  # Multi-process
+    max_generations = 50  # max generation
+    n_pop = 100  # n pop
 
     alg_names = ["HGA_CDM_IALS","HGA_CDM_ALS","HGA_CDM_LS","GA_CDM"]
     data_names = ["FrcSub",  "Math2", "Math1"]
-    alg_id = 0
-    dataset_id = 0
-    n_pop = 100
-    granularity = 1
+    # alg_id = 0
+    # dataset_id = 0
     NumTime = 0
     sum_ALS = 0
     for alg_id in range(3,4):
@@ -195,13 +179,8 @@ if __name__ == '__main__':
             print_info = str(NumTime) + "##" + str(alg_id) + " " + alg_name + " " + str(
                 dataset_id) + " " + data_name
             print(print_info)
-            data, q_matrix, q_matrix_coarse = prepare(dataset_id, granularity)
-
+            data, q_matrix = prepare(dataset_id)  # Read data
             n_questions, n_knowledge_coarse = np.mat(q_matrix).shape
-
             run_main(alg_name, data_name, data, q_matrix, max_runs, max_split, max_generations, n_pop, is_multi,
-                     n_knowledge_coarse)
-
-
-
-    print('总用时：' + str(time.time() - startTimeA) + '秒')
+                     n_knowledge_coarse)  # Main function entry
+    print('Total time：' + str(time.time() - startTimeA) + 's')
